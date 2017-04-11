22b4f37570a47e902dedf0f17cda7278ec5e36d2
fooofei <aihujianfei@qq.com>
2017-04-11 09:16:12 +0800

    1

--- requests_post.py
+++ requests_post.py
@@ -1,4 +1,4 @@
-﻿#coding=utf-8
+﻿# coding=utf-8
 '''
 记录我犯过的错误
 '''
@@ -7,23 +7,24 @@ import requests
 
 
 def upload():
-    url = 'http://base' # some url
+    url = 'http://base'  # some url
 
     # 随机的参数
-    param = {'arg0':'1',
-             'arg1':'2',}
+    param = {'arg0': '0',
+             'arg1': '1', }
 
     # 比如上传文件
     fullpath_upload = '/home/a/a'
 
-    with open(fullpath_upload,'rb') as f:
-        fs = {'file':f}
-        req = requests.post(url,data=param,files=fs)
+    with open(fullpath_upload, 'rb') as f:
+        fs = {'file': f}
+        req = requests.post(url, data=param, files=fs)
         assert (req.status_code == 200)
 
-    # 这里的 post 使用 data=param 参数把键值对的参数发送出去，这是放到 http post 中的 body(消息主体)中
-    # 如果使用 params=param 这是把键值对放到 url 中拼接组合起来，跟上面的不一样
-    # 要能准确区分服务器接收哪种发送请求，区分上面两种使用方法
+        # 这里的 post 使用 data=param 参数把键值对的参数发送出去，这是放到 http post 中的 body(消息主体)中
+        # 如果使用 params=param 这是把键值对放到 url 中拼接组合起来，跟上面的不一样
+        # 要能准确区分服务器接收哪种发送请求，区分上面两种使用方法
+
 
 def entry():
     upload()
