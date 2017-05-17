45401a48a96eb133fa69aa49812b83159b10e493
fooofei <aihujianfei@qq.com>
2017-05-17 09:56:10 +0800

    * others

--- counter_items.py
+++ counter_items.py
@@ -1,5 +1,12 @@
 #coding=utf-8
 
+'''
+
+    Python 中计数的方式
+    
+    如果不使用 collections.Counter 的话，会是这么用 counter_list_old_way()
+
+'''
 
 def counter_list():
     from collections import Counter
@@ -50,9 +57,20 @@ def counter_update_items2():
 
     b = Counter()
 
-    b.update({'a':'4'})
-    b.update({'a':'7'})
-    # b.update({'b':'1'}) # error
+
+    b.update({'a':'4'}) # 首次使用时 b 为空 'a' 直接使用 dict 的构造方式(update())填充进去 值为 '4'
+    b.update({'a':'7'}) # 取出 'a' 的值 '4' 在此基础上 + '7'， 为 '47'
+
+    # b.update({'b':'1'}) # error  # b 不为空，使用dict 的 get(val,default=0) 的方式 + '1' ， 出现 0 + '1' 导致异常
+
+    '''
+     same use
+     
+    b.update(a='4')
+    b.update(a='7')
+    b.update(b='1')
+    
+    '''
 
     print (b.items())
 
@@ -61,11 +79,27 @@ def counter_update_items2():
     
     '''
 
+def counter_list_old_way():
+    a = ['a', 'a', 'b', 'b',
+         'c', 'c', 'c']
+
+    b = {}
+
+    for e in a :
+        if not e in b:
+            b[e] = 1
+        else:
+            b[e] += 1
+
+    print (b.items())
+
+
 def entry():
     counter_list()
     counter_list2()
     counter_update_items()
     counter_update_items2()
+    counter_list_old_way()
 
 if __name__ == '__main__':
     entry()
