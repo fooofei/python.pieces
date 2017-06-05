3ea9d77f52a488c5b7eefe7367b86afe92def8fa
fooofei <aihujianfei@qq.com>
2017-06-05 14:29:15 +0800

    + add logger

---
+++ py_logger.py
@@ -0,0 +1,48 @@
+#coding=utf-8
+
+import os
+import logging
+
+
+def get_logger_1():
+    FORMAT = u'%(asctime)s %(name)-12s %(levelname)-8s %(message)s'
+
+    formatter = logging.Formatter(FORMAT)
+    handler = logging.StreamHandler()
+    handler.setFormatter(formatter)
+    log = logging.getLogger(os.path.basename(os.path.realpath(__file__)))
+    log.addHandler(handler)
+    log.setLevel(logging.DEBUG)
+    return log
+
+
+def get_logger_2():
+    FORMAT = u'%(asctime)s %(module)s %(funcName)s %(levelname)-8s %(message)s'
+    datefmt = u'%Y/%m/%d %H:%M:%S'
+    # >= level will be output
+    logging.basicConfig(format=FORMAT,level=logging.DEBUG,datefmt=datefmt)
+    log = logging.getLogger(__name__) # __main__
+    return log
+
+log = get_logger_2()
+
+
+
+
+
+
+def foo1():
+    log.info(u'logger info foo1()')
+    log.debug(u'logger debug foo1()')
+    log.warn(u'logger warn foo1()')
+    log.error(u'logger error foo1()')
+    print ('call foo1()')
+
+def entry():
+    log.info(u'logger info entry()')
+    log.debug(u'logger debug entry()')
+    print ('call entry()')
+    foo1()
+
+if __name__ == '__main__':
+    entry()
