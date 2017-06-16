b95482b1c67b3077116fc5c33cee13af26d8afc7
fooofei <aihujianfei@qq.com>
2017-06-16 14:10:26 +0800

    * closure  class

---
+++ closure_compare_class.py
@@ -0,0 +1,46 @@
+#coding=utf-8
+
+import unittest
+
+class MyObject(object):
+    def __init__(self):
+        self._value = 1
+
+    def increment(self):
+        self._value += 1
+        return self._value
+
+
+def closure_increment():
+
+    def _inner():
+        _value[0] += 1
+        return _value[0]
+
+    _value=[1]
+    return _inner  # Cannot be _inner()
+
+
+
+
+class MyTestCase(unittest.TestCase):
+
+    def test_equal(self):
+
+        obj = MyObject()
+        v1 = []
+        for _ in range(10):
+            v1.append(obj.increment())
+
+
+        closure = closure_increment()
+        v2 = []
+        for _ in range(10):
+            v2.append(closure())
+
+        self.assertEqual(v1, v2)
+
+
+
+if __name__ == '__main__':
+    unittest.main()
