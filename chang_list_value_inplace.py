61ec31b2d2f0973e86a215755b9a46f1fb42baf7
fooofei <aihujianfei@gmail.com>
2017-06-24 22:22:23 +0800

    * inplace

--- chang_list_value_inplace.py
+++ chang_list_value_inplace.py
@@ -1,25 +1,33 @@
 # coding=utf-8
 
+import os
+import sys
+import unittest
+from copy import deepcopy
 
-def foo1(l):
-    l = sorted(l)
+class MyTestCase(unittest.TestCase):
+
+    def _not_change(self, l):
+        l = sorted(l)
 
 
-def foo2(l):
-    l[::] = sorted(l)
+    def _change(self, l):
+        l[::] = sorted(l)
 
 
-def entry():
-    from copy import deepcopy
-    l1 = ['b', 'a', 'c', 'f', 'e', 'd']
-    l2 = deepcopy(l1)
+    def test_not_change(self):
+        l = ['b', 'a', 'c', 'f', 'e', 'd']
+        l2 = deepcopy(l)
+        self._not_change(l2)
+        self.assertEqual(l2, l)
 
-    foo1(l1)
-    print (l1)  # ['b', 'a', 'c', 'f', 'e', 'd']
 
-    foo2(l2)
-    print (l2)  # ['a', 'b', 'c', 'd', 'e', 'f']
+    def test_change(self):
+        l = ['b', 'a', 'c', 'f', 'e', 'd']
+        l2 = deepcopy(l)
+        self._change(l2)
+        self.assertEqual(l2, sorted(l))
 
 
 if __name__ == '__main__':
-    entry()
+    unittest.main()
