c04dd67fa59ea778183e4060b3352133e6ef9047
fooofei <aihujianfei@gmail.com>
2017-06-26 07:16:30 +0800

    *

--- collections_counter_items.py
+++ collections_counter_items.py
@@ -8,90 +8,92 @@
 
 '''
 
-def counter_list():
-    from collections import Counter
-    a = ['a','a','b','b',
+import os
+import sys
+from collections import Counter
+import unittest
+
+class MyTestCase(unittest.TestCase):
+
+    def test_counter_1(self):
+        a = ['a','a','b','b',
          'c','c','c']
 
-    b = Counter(a)
-    print (b.items())
-    '''
-    [('a', 2), ('c', 3), ('b', 2)]
-    '''
+        b = Counter(a)
 
+        self.assertEqual(b.items(), [('a', 2), ('c', 3), ('b', 2)] )
 
-def counter_list2():
-    from collections import Counter
+    def test_counter_2(self):
 
-    a = ['a', 'a', 'b', 'b',
-         'c', 'c', 'c']
-    b = Counter()
-
-    for e in a:
-        b.update({e:1})
-    print (b.items())
-
-    '''
-    [('a', 2), ('c', 3), ('b', 2)]
-    '''
-
-def counter_update_items():
-    from collections import Counter
-
-    b = Counter()
-
-    b.update({'a':4})
-    b.update({'a':7})
-    b.update({'b':2})
-    b.update({'b':2})
-    b.update({'c':1})
-    b.update({'c':0})
-
-    print (b.items())
-    '''
-    [('a', 11), ('c', 1), ('b', 4)]
-    '''
-
-def counter_update_items2():
-    from collections import Counter
-
-    b = Counter()
-
-
-    b.update({'a':'4'}) # 首次使用时 b 为空 'a' 直接使用 dict 的构造方式(update())填充进去 值为 '4'
-    b.update({'a':'7'}) # 取出 'a' 的值 '4' 在此基础上 + '7'， 为 '47'
-
-    # b.update({'b':'1'}) # error  # b 不为空，使用dict 的 get(val,default=0) 的方式 + '1' ， 出现 0 + '1' 导致异常
-
-    '''
-     same use
-     
-    b.update(a='4')
-    b.update(a='7')
-    b.update(b='1')
-    
-    '''
-
-    print (b.items())
-
-    '''
-    [('a', '47')]
-    
-    '''
-
-def counter_list_old_way():
-    a = ['a', 'a', 'b', 'b',
+        a = ['a', 'a', 'b', 'b',
          'c', 'c', 'c']
 
-    b = {}
+        b = Counter()
 
-    for e in a :
-        if not e in b:
-            b[e] = 1
-        else:
-            b[e] += 1
+        for e in a :
+            b.update({e:1})
 
-    print (b.items())
+        self.assertEqual(b.items(), [('a', 2), ('c', 3), ('b', 2)])
+
+
+    def test_counter_3(self):
+        b = Counter()
+
+        b.update({'a': 4})
+        b.update({'a': 7})
+        b.update({'b': 2})
+        b.update({'b': 2})
+        b.update({'c': 1})
+        b.update({'c': 0})
+
+        self.assertEqual(b.items(), [('a', 11), ('c', 1), ('b', 4)])
+
+    def test_counter_4(self):
+        b = Counter()
+
+        b.update(a = 4)
+        b.update(a = 7)
+        b.update(b = 2)
+        b.update(b = 2)
+        b.update(c = 1)
+        b.update(c = 0)
+
+        self.assertEqual(b.items(), [('a', 11), ('c', 1), ('b', 4)])
+
+    def test_counter_error(self):
+        b = Counter()
+        b.update({'a': '4'})  # 首次使用时 b 为空 'a' 直接使用 dict 的构造方式(update())填充进去 值为 '4'
+        b.update({'a': '7'})  # 取出 'a' 的值 '4' 在此基础上 + '7'， 为 '47'
+
+        self.assertEqual(b.items(), [('a','47')])
+
+        with self.assertRaises(TypeError):
+            b.update({'a':9})
+
+        with self.assertRaises(TypeError):
+            b.update({'b':'1'}) # b 不为空，使用dict 的 get(val,default=0) 的方式 + '1' ， 出现 0 + '1' 导致异常
+
+
+    def test_counter_old_way(self):
+        a = ['a', 'a', 'b', 'b',
+             'c', 'c', 'c']
+
+        b = {}
+
+        for e in a:
+            if not e in b:
+                b[e] = 1
+            else:
+                b[e] += 1
+
+
+        c = Counter(a)
+
+        self.assertEqual(b.items(), c.items())
+
+        self.assertEqual(b.items(), [('a',2),('b',2),('c',3)])
+
+
 
 
 def sum_all_item_count():
@@ -124,4 +126,4 @@ def entry():
     sum_all_item_count()
 
 if __name__ == '__main__':
-    entry()
+    unittest.main()
