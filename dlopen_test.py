615bf59716175f1b454bb2fcbc6835e24368bb10
fooofei <aihujianfei@qq.com>
2017-07-14 09:29:05 +0800

    + dlopen

---
+++ dlopen_test.py
@@ -0,0 +1,33 @@
+#coding=utf-8
+
+'''
+#define 	dl_restrict
+#define 	RTLD_LAZY   0x1
+#define 	RTLD_NOW   0x2
+#define 	RTLD_LOCAL   0x4
+#define 	RTLD_GLOBAL   0x8
+#define 	RTLD_NOLOAD   0x10
+#define 	RTLD_NODELETE   0x80
+#define 	RTLD_NEXT   ((void *) -1)
+#define 	RTLD_DEFAULT   ((void *) -2)
+
+
+'''
+
+import os
+import sys
+import ctypes
+
+
+def entry():
+    # if so has undefined symbol, this also can load success.
+    # 当遇到未定义符号的函数会报错误 python: symbol lookup error: xxxx.so: undefined symbol: xxxx
+
+    a = ctypes.CDLL('xxxxx.so',mode=1)
+
+    # ctypes.CDLL('xxxx.so')
+    # == ctypes.CDLL('xxxx.so',mode=2)
+
+
+if __name__ == '__main__':
+    entry()
