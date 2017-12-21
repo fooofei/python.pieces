d5201a02706c76b6ba8ecf0873230a557cf5cd37
fooofei <aihujianfei@qq.com>
2017-12-21 10:39:09 +0800

    add kill

--- subprocess_timeout.py
+++ subprocess_timeout.py
@@ -29,6 +29,39 @@ def popen_timeout(popen_ins, timeout):
   return True
 
 
+def popen_kill(popen_ins):
+  '''
+  kill process created by subprocess.Popen()
+  :param popen_ins:
+  :return:  None
+  '''
+  if popen_ins.poll() is None:
+    #print('kill proc {}'.format(popen_ins.pid))
+    # popen_ins.terminate()
+    popen_ins.kill()
+
+  while popen_ins.poll()  is None:
+    #print('fail kill, trying') # will be many times
+    # if too fast, will be error OSError: [Errno 3] No such process
+    os.kill(popen_ins.pid, 9)
+    time.sleep(3)
+
+
+def timeout_subprocess(*args,**kwargs):
+  '''
+  when wrap subprocess.Popen() as this,
+  the Popen() param will not have auto complete
+  '''
+  timeout=kwargs.pop('timeout',None)
+  p_ins = subprocess.Popen(*args,**kwargs)
+  if timeout is not None:
+    if not popen_timeout(p_ins,timeout):
+      # timeout
+      popen_kill(p_ins)
+  return p_ins
+
+
+
 
 
 
