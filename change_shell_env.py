1cfdb2214cc35b0e9be376c6ca2513a792c4d40b
fooofei <aihujianfei@qq.com>
2018-02-27 19:07:41 +0800

    u

---
+++ change_shell_env.py
@@ -0,0 +1,19 @@
+#coding=utf-8
+
+'''
+The file shows how to change the login shell envrionment
+'''
+
+
+
+# ref https://stackoverflow.com/questions/38982640/set-shell-environment-variable-via-python-script
+# ref https://stackoverflow.com/questions/35780715/setting-environment-variables-of-parent-shell-in-python
+# ref https://stackoverflow.com/questions/8365394/set-environment-variable-in-python-script
+
+def entry():
+  # no way
+  pass
+
+
+if __name__ == '__main__':
+    entry()
