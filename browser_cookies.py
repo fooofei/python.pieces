ae1aa1fd28e291c3f95690b487f90fc24bdca9bb
fooofei <aihujianfei@gmail.com>
2018-08-06 15:06:17 +0800

    cookie

--- browser_cookies.py
+++ browser_cookies.py
@@ -2,6 +2,9 @@
 
 '''
 the file shows how to get cookies from Chrome browser installed machine.
+
+pycookiecheat 只支持 macOS linux 只支持 python3+
+
 '''
 
 import os
@@ -23,6 +26,16 @@ def _get_chrome_cookies():
   '''
   return type is RequestsCookieJar
   on macOS, may need use authority check
+
+  使用这个库需要修改 这个库的__init__.py
+  fh = tempfile.NamedTemporaryFile(mode='wb', delete=False) # delete=False 不要关闭就删除文件
+  # NamedTemporaryFile 就已经打开文件了 不要再次打开文件 会打开失败
+  tmp_cookie_file = fh.name
+  ...
+  fh.close() # 要关闭是为了后面 sqlite 能打开
+
+  这个库会丢到 cookie ，能在chrome中看到的cookie 到这里就看不到了
+
   '''
   import browsercookie
   from itertools import chain
