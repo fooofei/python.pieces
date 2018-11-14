4ab5070d686968a1c6eadb9466a07bc508d6865a
fooofei <aihujianfei@qq.com>
2018-11-14 13:54:19 +0800

    json encode datetime

--- sets_to_json.py
+++ sets_to_json.py
@@ -15,9 +15,25 @@ class JSONSetEncoder(json.JSONEncoder):
     return super(JSONSetEncoder, self).default(o)
 
 
+# or
+'''
+  File "/usr/lib64/python2.7/json/encoder.py", line 184, in default
+    raise TypeError(repr(o) + " is not JSON serializable")
+TypeError: datetime.datetime(2018, 11, 14, 2, 59, 49, 970355) is not JSON serializable
+'''
+
+class JSONDtmEncoder(JSONEncoder):
+    def default(self, o):
+        if isinstance(o, datetime.datetime):
+            return '{}'.format(o)
+        return super(JSONDtmEncoder, self).default(o)
+
 
 
 
 def use():
   v = set()
   v1 = json.dumps(v,cls=JSONSetEncoder)
+  
+  v = {'timestamp': datetime.datetime.now()}
+  v2 = json.dumps(v, cls=JSONDtmEncoder)
