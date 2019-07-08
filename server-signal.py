c1f4eb6ff5dc94ec7f492cab53f868d06a5c6a1e
fooofei <aihujianfei@gmail.com>
2019-07-08 21:36:14 +0800

    signal

--- server-signal.py
+++ server-signal.py
@@ -1,9 +1,39 @@
 #coding=utf-8
 
+'''
+这个文件用来记录模板：
+一个后台服务，应该忽略哪些信号
+'''
+
 import os
 import sys
 import signal
 import time
+
+
+def daemon_stop_signals(signum, frame):
+    '''
+    If process kill by some signals, then execute here
+    实现  CTRL+C 或者被 kill （不同 signal） 也能正常执行某段代码
+    '''
+    print('[+] Recv signal num={n}, daemon will exit.'.format(n=signum))
+    sys.exit(0)  # this will break while
+
+def test_kill():
+    # SIGTERM is systemctrl stop sensor signal
+    signal.signal(signal.SIGTERM, daemon_stop_signals)
+    # SIGINT is CTRL+C
+    # signal.signal(9, daemon_stop_signals) #signal.SIGKILL # cannot be catch
+    signal.signal(signal.SIGILL, daemon_stop_signals)
+    signal.signal(signal.SIGABRT, daemon_stop_signals)
+    signal.signal(3, daemon_stop_signals)  # signal.SIGQUIT
+
+
+    try:
+        time.sleep(20000)
+    finally:
+        print("do cleanup here")
+
 
 def _signal_handler(sigNum, frame):
     sys.exit(0)
