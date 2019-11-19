289f35c202cb6535ba2f5fdd616828cf566d4b55
fooofei <aihujianfei@qq.com>
2019-11-19 16:51:54 +0800

    Update datetime_timestamp.py

--- datetime_timestamp.py
+++ datetime_timestamp.py
@@ -1,4 +1,4 @@
-#coding=utf-8
+# coding=utf-8
 
 from datetime import datetime
 from timeit import default_timer as time
@@ -8,16 +8,19 @@ from calendar import nextmonth
 from calendar import prevmonth
 from calendar import monthlen
 
+
 def datetime2unix(dt):
     v = mktime(dt.timetuple())
     return int(v)
 
+
 def datetime2unixmill(dt):
-    return 1000*datetime2unix(dt)
+    return 1000 * datetime2unix(dt)
 
 
 def unix2datetime(t):
     return datetime.fromtimestamp(t)
+
 
 def monthdelta(date: datetime, delta: int) -> datetime:
     '''
@@ -28,17 +31,32 @@ def monthdelta(date: datetime, delta: int) -> datetime:
     '''
     new_year, new_month = date.year, date.month
     func_offmonth = prevmonth if delta < 0 else nextmonth
-    for _ in range(0,abs(delta)):
+    for _ in range(0, abs(delta)):
         new_year, new_month = func_offmonth(new_year, new_month)
     new_day = min(date.day, monthlen(new_year, new_month))
     return date.replace(day=new_day, month=new_month, year=new_year)
+
 
 def from_mysql_todays(days):
     '''
     :param days: int
     :return: datetime
     '''
-    return datetime.fromordinal(days-365)
+    return datetime.fromordinal(days - 365)
+
+
+def from_str(dt_str):
+    '''
+    a = datetime.now()
+    b = from_str(str(a.isoformat()))
+    :param dt_str:
+    :return:
+    '''
+    dt, _, us = dt_str.partition(".")
+    dt = datetime.strptime(dt, "%Y-%m-%dT%H:%M:%S")
+    us = int(us.rstrip("Z"), 10)
+    return dt + timedelta(microseconds=us)
+
 
 class MyTest(unittest.TestCase):
     def test1(self):
@@ -49,12 +67,13 @@ class MyTest(unittest.TestCase):
     def test2(self):
         a = datetime(year=2019, month=1, day=31)
         b = monthdelta(a, 1)
-        self.assertEqual(b, datetime(year=2019,month=2, day=28))
+        self.assertEqual(b, datetime(year=2019, month=2, day=28))
 
     def test_mysql_todays(self):
         a = 737928
         b = from_mysql_todays(a)
         self.assertEqual(b, datetime(year=2020, month=5, day=18))
+
 
 if __name__ == '__main__':
     a = unix2datetime(1555430400)
