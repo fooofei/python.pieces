46d83f66d52e7f67f2c2265dc8ae3fb38340fc6f
fooofei <aihujianfei@qq.com>
2021-01-10 21:35:28 +0800

    Update datetime_timestamp.py

--- datetime_timestamp.py
+++ datetime_timestamp.py
@@ -4,10 +4,14 @@ from datetime import datetime
 from timeit import default_timer as time
 from time import mktime
 import unittest
-from calendar import nextmonth
-from calendar import prevmonth
-from calendar import monthlen
+from calendar import _prevmonth as prevmonth
+from calendar import _monthlen as monthlen
 from datetime import timedelta
+from calendar import _nextmonth as nextmonth
+from iso8601 import parse_date as parse_iso8601
+from rfc3339 import format as format_rfc3389
+from rfc3339 import format_millisecond as format_rfc3389_mill
+from rfc3339 import format_microsecond as format_rfc3389_micro
 
 
 def datetime2unix(dt):
@@ -46,17 +50,28 @@ def from_mysql_todays(days):
     return datetime.fromordinal(days - 365)
 
 
-def from_str(dt_str):
+def parse_gmt(date_str):
     '''
-    a = datetime.now()
-    b = from_str(str(a.isoformat()))
-    :param dt_str:
+    parse datetime like "2021-01-07T08:53:22.022GMT" to datetime.datetime(2021, 1, 7, 8, 53, 22, 22000)
+    :param date_str:
     :return:
     '''
-    dt, _, us = dt_str.partition(".")
-    dt = datetime.strptime(dt, "%Y-%m-%dT%H:%M:%S")
-    us = int(us.rstrip("Z"), 10)
-    return dt + timedelta(microseconds=us)
+    date_str = date_str.rstrip("GMT")
+    return parse_iso8601(date_str)
+
+
+def close_at(a, b):
+    '''
+    时间是否接近
+    a b are all datetime
+    :param a:
+    :param b:
+    :return:
+    '''
+    a = a.replace(tzinfo=None)
+    b = b.replace(tzinfo=None)
+    c = b - a
+    return abs(c.total_seconds()) < 15
 
 
 class MyTest(unittest.TestCase):
@@ -74,6 +89,33 @@ class MyTest(unittest.TestCase):
         a = 737928
         b = from_mysql_todays(a)
         self.assertEqual(b, datetime(year=2020, month=5, day=18))
+
+    def test_ios8601(self):
+        src1 = datetime.now().astimezone()
+        a = src1.isoformat()  # 2021-01-10T21:27:54.287055+08:00
+        dst1 = parse_iso8601(a)
+        self.assertEqual(src1, dst1)
+
+    def test_rfc3339(self):
+        src1 = datetime.now().astimezone()
+        a = format_rfc3389(src1)  # 2021-01-10T21:28:24+08:00
+        dst1 = parse_iso8601(a)
+        # 等于去掉毫秒
+        src1 = src1.replace(microsecond=0)
+        self.assertEqual(src1, dst1)
+
+    def test_rfc3339_2(self):
+        src1 = datetime.now().astimezone()
+        a = format_rfc3389_micro(src1)  # 2021-01-10T21:28:38.490201+08:00
+        dst1 = parse_iso8601(a)
+        self.assertEqual(src1, dst1)
+
+    def test_parse_gmt(self):
+        a = "2021-01-07T08:53:22.022GMT"
+        a1 = "2021-01-07 08:53:22.022000+00:00"
+        b = parse_gmt(a)
+        b1 = parse_iso8601(a1)
+        self.assertEqual(b, b1)
 
 
 if __name__ == '__main__':
